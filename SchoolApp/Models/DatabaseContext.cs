bd4057018ce115cd55f359ac94ca796a2cfd4390
etheoo <etheoo@protonmail.com>
2023-12-03 15:46:25 +0100

    Update connection string

--- SchoolApp/Models/DatabaseContext.cs
+++ SchoolApp/Models/DatabaseContext.cs
@@ -46,8 +46,7 @@ public partial class DatabaseContext : DbContext
     public virtual DbSet<YearGroup> YearGroups { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("Data Source=Database.db");
+        => optionsBuilder.UseSqlite("Data Source=../../../Database.db");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
