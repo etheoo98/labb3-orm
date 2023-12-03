abe6ef925ee5e831792cc40d70c31b9dd02a7dab
etheoo <etheoo@protonmail.com>
2023-12-03 09:19:12 +0100

    Remove role creation from AdminService.cs

--- SchoolApp/Services/AdminService.cs
+++ SchoolApp/Services/AdminService.cs
@@ -9,16 +9,9 @@ public class AdminService(DatabaseContext context) : IAdminService
             return false;
         }
         
-        if (!context.Roles.Any(r => r.Name == "Administrator"))
-        {
-            context.Roles.Add(new Role { Name = "Administrator" });
-            context.SaveChanges();
-        }
-        
         var passwordHash = BCrypt.Net.BCrypt.EnhancedHashPassword(password, 11);
         var admin = new Administrator
         {
-            RoleName = "Administrator",
             Username = username,
             Password = passwordHash
         };
